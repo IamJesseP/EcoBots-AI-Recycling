13cdbf3c5ac452e829c996a156a3d104ff17d3a3
Jesse <jesseorhs@gmail.com>
2023-08-04 15:59:16 -0700

    feat: add footer content and styling

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -1,5 +1,16 @@
 import React from 'react';
-
+import EcoBotsLogoWhite from '../assets/EcoBotsLogoWhite.png';
 export default function Footer() {
-  return <footer id="footer">Footer</footer>;
+  return (
+    <footer id="footer">
+      <div className="footer-section">
+        <img src={EcoBotsLogoWhite}></img>
+        <ul>
+          <li>About us</li>
+          <li>FAQs</li>
+          <li>Academy</li>
+        </ul>
+      </div>
+    </footer>
+  );
 }

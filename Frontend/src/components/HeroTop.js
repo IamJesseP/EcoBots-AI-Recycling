03f08c72c9a97bf9628500fbb17f171c880b3b1d
Jesse <jesseorhs@gmail.com>
2023-08-04 12:28:48 -0700

    feat: add hero quote and styling

--- Frontend/src/components/HeroTop.js
+++ Frontend/src/components/HeroTop.js
@@ -2,5 +2,16 @@ import React from 'react';
 import '../styles.css';
 
 export default function HeroTop() {
-  return <div id="hero-top">HeroTop</div>;
+  return (
+    <div id="hero-top">
+      <div className="hero-quote">
+        An{' '}
+        <span style={{ color: '#49A078', fontStyle: 'italic', fontFamily: 'Andada Pro' }}>
+          AI-Eco
+        </span>{' '}
+        <button className="hero-button">solution </button> designed to quantify your recycling
+        impact
+      </div>
+    </div>
+  );
 }

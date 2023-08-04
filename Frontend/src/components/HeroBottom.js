cfa6b09664ac348d6fcde7df4db1b7ebae13adbd
Jesse <jesseorhs@gmail.com>
2023-08-04 13:15:01 -0700

    feat: add hero card section content/style
    This commit adds the image cards to the hero section and accounts for
    responsiveness.

--- Frontend/src/components/HeroBottom.js
+++ Frontend/src/components/HeroBottom.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import '../styles.css';
+import BottlesCard from '../assets/BottlesCard.png';
+import CardBoardCard from '../assets/CardBoardCard.png';
+import GreenPitchCard from '../assets/GreenPitchCard.png';
+import RecycleBinCard from '../assets/RecycleBinCard.png';
 
 export default function HeroBottom() {
-  return <div id="hero-bottom">HeroBottom</div>;
+  return (
+    <div id="hero-bottom">
+      <div className="hero-card-container">
+        <img src={CardBoardCard}></img>
+        <img src={RecycleBinCard}></img>
+        {/* TO DO: Fix pixelated svg green pitch card */}
+        <img src={GreenPitchCard}></img>
+        <img src={BottlesCard}></img>
+      </div>
+    </div>
+  );
 }

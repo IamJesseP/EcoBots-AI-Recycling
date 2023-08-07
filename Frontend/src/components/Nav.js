ac44b953c4b072a0b9f07889261db16fdce77a23
Jesse <jesseorhs@gmail.com>
2023-08-07 09:48:03 -0700

    feat: give navbar sticky, stylings

--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -1,54 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import EcoBotsLogo from '../assets/EcoBotsLogo.png';
 import { Link as LinkScroll } from 'react-scroll';
 import { Link as LinkRouter } from 'react-router-dom';
 
 export default function Nav() {
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setHasScrolled(currentScrollPos > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav id="nav">
-      <div className="navbar-section">
-        <LinkScroll
-          className="logo"
-          to="nav"
-          spy={true}
-          duration={1200}
-          smooth={true}
-          offset={0}
-          style={{ cursor: 'pointer' }}>
-          <img src={EcoBotsLogo}></img>
-        </LinkScroll>
-      </div>
-      <ul className="navbar-section">
-        <li>
+    <>
+      <nav id="nav" className={hasScrolled ? 'scrolled' : ''}>
+        <div className="navbar-section">
           <LinkScroll
-            to="instructional"
-            className="nav-item"
+            className="logo"
+            to="hero-top"
             spy={true}
             duration={1200}
             smooth={true}
-            offset={-80}
+            offset={-200}
             style={{ cursor: 'pointer' }}>
-            How It Works
+            <img src={EcoBotsLogo}></img>
           </LinkScroll>
-        </li>
-        <li>
-          <LinkScroll
-            to="about"
-            className="nav-item"
-            spy={true}
-            duration={1200}
-            smooth={true}
-            offset={0}
-            style={{ cursor: 'pointer' }}>
-            About
-          </LinkScroll>
-        </li>
-        <li>FAQ</li>
-      </ul>
-      <div className="navbar-section-2">
-        <button className="hover">Log In</button>
-        <button className="hover">Recycle</button>
-      </div>
-    </nav>
+        </div>
+        <ul className="navbar-section">
+          <li>
+            <LinkScroll
+              to="instructional"
+              className="nav-item"
+              spy={true}
+              duration={1200}
+              smooth={true}
+              offset={-80}
+              style={{ cursor: 'pointer' }}>
+              How It Works
+            </LinkScroll>
+          </li>
+          <li>
+            <LinkScroll
+              to="about"
+              className="nav-item"
+              spy={true}
+              duration={1200}
+              smooth={true}
+              offset={0}
+              style={{ cursor: 'pointer' }}>
+              About
+            </LinkScroll>
+          </li>
+          <li>FAQ</li>
+        </ul>
+        <div className="navbar-section-2">
+          <button className="hover">Log In</button>
+          <button className="hover">Recycle</button>
+        </div>
+      </nav>
+    </>
   );
 }

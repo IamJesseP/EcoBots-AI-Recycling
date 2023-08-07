908c4b669c674acbc3e660a540e0495c3f049c40
Jesse <jesseorhs@gmail.com>
2023-08-07 12:05:50 -0700

    feat: finish input page content and styling
    This commit adds the content and styling for the input page. Including
    the spline model and an upload image button ready for backend
    connection.

---
+++ Frontend/src/components/Input.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Spline from '@splinetool/react-spline';
+import { BsUpload } from 'react-icons/bs';
+
+export default function Input() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    handleSubmit(event, file); // Pass the selected file to handleSubmit
+  };
+
+  const handleSubmit = async (event, file) => {
+    event.preventDefault();
+
+    if (!file) {
+      alert('Please select a file.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('photo', file);
+    try {
+      const response = await fetch('YOUR_BACKEND_ENDPOINT', {
+        method: 'POST',
+        body: formData
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('There was an error uploading the file.', error);
+    }
+  };
+
+  return (
+    <div className="input-page">
+      <h2>Make Every Recycle Count</h2>
+      <h1>A new way to understand your impact</h1>
+      <div className="spline">
+        <Spline scene="https://prod.spline.design/QgyFsAcjnjNZv-zO/scene.splinecode" />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          onChange={handleFileChange}
+          accept="image/*"
+          id="fileInput"
+          style={{ display: 'none' }}
+          className="hover"
+        />
+        <label htmlFor="fileInput" className="file-upload pointer">
+          <BsUpload className="file-upload-icon" />
+          Upload
+        </label>
+        <span>{selectedFile ? selectedFile.name : ''}</span>
+      </form>
+    </div>
+  );
+}
--- Frontend/src/components/Recycle.js
+++ Frontend/src/components/Recycle.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import Nav from './Nav';
+import Footer from './Footer';
+import Input from './Input';
+import Output from './Output';
 
 export default function Recycle() {
-  return <div>test</div>;
+  return (
+    <div id="recycle">
+      <Nav routed={'recycle'} />
+      <div className="recycle-page">
+        <Input />
+        <Output />
+      </div>
+      <Footer />
+    </div>
+  );
 }

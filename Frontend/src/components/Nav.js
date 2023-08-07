549bc630feade4b58125e21ef2e43a77ce908ef6
Jesse <jesseorhs@gmail.com>
2023-08-07 14:32:51 -0700

    feat: conditionally render nav
    This commmit adds logic to the navbar to conditionally render based on
    routing location.

--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import EcoBotsLogo from '../assets/EcoBotsLogo.png';
 import { Link as LinkScroll } from 'react-scroll';
-import { Link as LinkRouter } from 'react-router-dom';
+import { Link as LinkRouter, useLocation } from 'react-router-dom';
 
 export default function Nav() {
   const [hasScrolled, setHasScrolled] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -22,44 +23,51 @@ export default function Nav() {
     <>
       <nav id="nav" className={hasScrolled ? 'scrolled' : ''}>
         <div className="navbar-section">
-          <LinkScroll
-            className="logo"
-            to="hero-top"
-            spy={true}
-            duration={1200}
-            smooth={true}
-            offset={-200}
-            style={{ cursor: 'pointer' }}>
-            <img src={EcoBotsLogo}></img>
-          </LinkScroll>
+          {location.pathname === '/' && (
+            <LinkScroll
+              className="logo"
+              to="hero-top"
+              smooth={true}
+              offset={-200}
+              style={{ cursor: 'pointer' }}>
+              <img src={EcoBotsLogo}></img>
+            </LinkScroll>
+          )}
+          {location.pathname === '/recycle' && (
+            <LinkRouter className="logo" to="/" style={{ cursor: 'pointer' }}>
+              <img src={EcoBotsLogo}></img>
+            </LinkRouter>
+          )}
         </div>
-        <ul className="navbar-section">
-          <li>
-            <LinkScroll
-              to="instructional"
-              className="nav-item"
-              spy={true}
-              duration={1200}
-              smooth={true}
-              offset={-80}
-              style={{ cursor: 'pointer' }}>
-              How It Works
-            </LinkScroll>
-          </li>
-          <li>
-            <LinkScroll
-              to="about"
-              className="nav-item"
-              spy={true}
-              duration={1200}
-              smooth={true}
-              offset={0}
-              style={{ cursor: 'pointer' }}>
-              About
-            </LinkScroll>
-          </li>
-          <li>FAQ</li>
-        </ul>
+        {location.pathname === '/' && (
+          <ul className="navbar-section">
+            <li>
+              <LinkScroll
+                to="instructional"
+                className="nav-item"
+                spy={true}
+                duration={1200}
+                smooth={true}
+                offset={-80}
+                style={{ cursor: 'pointer' }}>
+                How It Works
+              </LinkScroll>
+            </li>
+            <li>
+              <LinkScroll
+                to="about"
+                className="nav-item"
+                spy={true}
+                duration={1200}
+                smooth={true}
+                offset={0}
+                style={{ cursor: 'pointer' }}>
+                About
+              </LinkScroll>
+            </li>
+            <li>FAQ</li>
+          </ul>
+        )}
         <div className="navbar-section-2">
           {/* <button className="hover">Log In</button> */}
           <LinkRouter to="/recycle">

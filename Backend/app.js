9ef32a861bf45d0c640d636c99f9f94b8b12d53f
Jesse <jesseorhs@gmail.com>
2023-08-09 16:27:51 -0700

    feat: implement security packages

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,33 @@
 import express from 'express';
+
+//security packages
 import cors from 'cors';
+import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
+import xss from 'xss-clean';
 // Routers
 import recycleRouter from './routes/recycleRoutes.js';
 import openAIRouter from './routes/openAIRoutes.js';
 
+var corsOptions = {
+  origin: '*', //  frontend domain
+  optionsSuccessStatus: 200,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+};
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
 const app = express();
+
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(xss());
+app.use(limiter);
+
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: '*', // allow any origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization']
-  })
-);
-
 // Endpoints
 app.use('/recycle', recycleRouter);
 app.use('/AI', openAIRouter);
